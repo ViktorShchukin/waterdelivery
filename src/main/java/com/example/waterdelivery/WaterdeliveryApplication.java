bb9f319fe25a6801af294dc7c3519690c00a3f61
anorisno <shukin.viktor22@gmail.com>
2026-01-26 07:35:31 +0300

    Add spring boot app base.

---
+++ src/main/java/com/example/waterdelivery/WaterdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.waterdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterdeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/waterdelivery/WaterdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.waterdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
